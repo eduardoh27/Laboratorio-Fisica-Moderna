31a40a1bc08d31b0dc75d54018d812e2a0581f44
eduardoh27 <78046940+eduardoh27@users.noreply.github.com>
2022-02-21 21:26:49 -0500

    fin análisis preliminar

--- Exp2 - Espectrografía/experimento2.py
+++ Exp2 - Espectrografía/experimento2.py
@@ -12,125 +12,81 @@ def main():
     #plt.style.use('science')
     plt.style.use(['science','no-latex'])
     
-    ruta = r"C:\Users\Eduardo\Downloads\DatosExp111.csv"
-    my_path = os.path.abspath(r"C:\Users\Eduardo\Downloads")
-    
-    i_rojo, v_rojo, i_verde, v_verde, i_amarillo, v_amarillo, i_azul, v_azul = np.loadtxt(ruta, unpack=True, delimiter=',', skiprows=1)
+    # violeta, azul, rojo
+    pixels_list = np.array([1190,1337,1845]) # x
+    pixels_list = np.array([0,1190,1337,1845]) # x
+    pixels_list = np.array([0, 1/4 - 1/36,1/4 - 1/25,1/4 - 1/9])
+    #lambdas_list = [0,410e-9,434e-9,656e-9] # y 
+    #lambdas_list = [0,410,434,656] # y
+    lambdas_list = [0,1/(410e-9),1/(434e-9),1/(656e-9)] # y
+    #lambdas_list = [1/(410),1/(434),1/(656)] # y
 
-    i_rojo *= 1e-8
-    i_amarillo *= 1e-8
-    i_azul *= 1e-8
-    i_verde *= 1e-8
-    
-    error_y = 0.001
-    error_x = 0.25e-8
-    
 
+    # 1. Calibración de líneas espectrales del hidrógeno
+    
+    pixels_list = np.array([0,1190,1337,1845]) # x
+    lambdas_list = [0,410,434,656] # y
+    
     fig = plt.figure()
     ax = fig.add_subplot()
-    ax.set_xlabel(r'$Corriente\,[A]$',fontsize=10)
-    ax.set_ylabel(r'$Voltaje\,[V]$',fontsize=10)
+    ax.set_xlabel(r'$Pixeles$',fontsize=10)
+    ax.set_ylabel(r'$Longitud\,de\,onda\,[nm]$',fontsize=10)
+    ax.set_title(r'Calibración de líneas espectrales de H',fontsize=10)
 
-    ax.scatter(i_rojo, v_rojo, c='r',s=13,label='Datos LED rojo')
-    plt.errorbar(i_rojo,v_rojo,yerr=error_y,xerr=error_x,fmt='r', ecolor='r')
-    regr_rojo = stats.linregress(i_rojo,v_rojo)
-    Y_rojo = i_rojo*regr_rojo.slope + regr_rojo.intercept
-    plt.plot(i_rojo, Y_rojo, '--',color='darkred', label='Ajuste LED rojo')
+    ax.scatter(pixels_list, lambdas_list, c='k',s=10)
+    #plt.errorbar(i_rojo,v_rojo,yerr=error_y,xerr=error_x,fmt='r', ecolor='r')
+    regression = stats.linregress(pixels_list,lambdas_list)
+    Y_regression = pixels_list*regression.slope + regression.intercept
+    plt.plot(pixels_list, Y_regression, '--',color='darkred', label=r'$\lambda$ = 0.35 pixeles - 6.83')
     
-    ax.scatter(i_amarillo, v_amarillo, c='yellow',s=13, label='Datos LED amarillo')
-    plt.errorbar(i_amarillo,v_amarillo,yerr=error_y,xerr=error_x,fmt='yellow', ecolor='yellow')
-    regr_amarillo = stats.linregress(i_amarillo,v_amarillo)
-    Y_amarillo = i_amarillo*regr_amarillo.slope + regr_amarillo.intercept
-    plt.plot(i_amarillo, Y_amarillo, '--',color='gold', label='Ajuste LED amarillo')
+    print(f'm = {regression.slope}, b = {regression.intercept}')
     
+     
     plt.legend()
-    plt.savefig('rojo-amarillo.png', dpi=300, bbox_inches='tight')
-    #plt.show()
-    
-    
-    
-    
-    
-    
-    fig1 = plt.figure()
-    ax1 = fig1.add_subplot()
-    ax1.set_xlabel(r'$Corriente\,[A]$',fontsize=10)
-    ax1.set_ylabel(r'$Voltaje\,[V]$',fontsize=10)
-
-    ax1.scatter(i_azul, v_azul, c='dodgerblue',s=13,label='Datos LED azul')
-    plt.errorbar(i_azul,v_azul,yerr=error_y,xerr=error_x,fmt='dodgerblue', ecolor='dodgerblue')
-    regr_azul = stats.linregress(i_azul,v_azul)
-    Y_azul = i_azul*regr_azul.slope + regr_azul.intercept
-    plt.plot(i_azul, Y_azul, '--',color='blue', label='Ajuste LED azul')
-    
-    ax1.scatter(i_verde, v_verde, c='lime',s=13, label='Datos LED verde')
-    plt.errorbar(i_verde,v_verde,yerr=error_y,xerr=error_x,fmt='lime', ecolor='lime')
-    regr_verde = stats.linregress(i_verde,v_verde)
-    Y_verde = i_verde*regr_verde.slope + regr_verde.intercept
-    plt.plot(i_verde, Y_verde, '--',color='green', label='Ajuste LED verde')
-    
-    plt.legend()
-    #plt.show()
-    plt.savefig("azul-verde.png")
-    
-
-    print(f"rojo: {regr_rojo.slope} +- {regr_rojo.stderr}, {regr_rojo.intercept} +- {regr_rojo.intercept_stderr}.")
-    print(f"amarillo: {regr_amarillo.slope} +- {regr_amarillo.stderr}, {regr_amarillo.intercept} +- {regr_amarillo.intercept_stderr}.")
-    print(f"azul: {regr_azul.slope} +- {regr_azul.stderr}, {regr_azul.intercept} +- {regr_azul.intercept_stderr}.")
-    print(f"verde: {regr_verde.slope} +- {regr_verde.stderr}, {regr_verde.intercept} +- {regr_verde.intercept_stderr}.")
-    
-    q = 1.602176634e-19
-    
-    valores_y = np.array([regr_azul.intercept*q,regr_verde.intercept*q,regr_amarillo.intercept*q,regr_rojo.intercept*q])
-    
-    long_azul = 469e-9 #nm
-    long_verde = 567e-9
-    long_amarillo = 590e-9
-    long_rojo = 659e-9
-    
-    c_luz =  299792458 #m/s
-    
-    """
-    Valores viejos
-    freq_azul = 6.66e14
-    freq_verde = 5.45e14
-    freq_amarillo = 5.16e14
-    freq_rojo = 4.62e14
-    # http://academic.eb.com/levels/
-    # https://www.britannica.com/science/color/The-visible-spectrum
-    """
-    freq_azul = c_luz/long_azul
-    freq_verde= c_luz/long_verde
-    freq_amarillo = c_luz/long_amarillo
-    freq_rojo = c_luz/long_rojo
-    
-  
-    
-    valores_x = np.array([freq_azul, freq_verde, freq_amarillo, freq_rojo])
-    print(valores_x)
-    
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot()
-    ax2.set_ylabel(r'$ \mathrm{Energía\,cinética\,de\,los\,electrones}\, [J]$',fontsize=10)
-    ax2.set_xlabel(r'$Frecuencia\,[Hz]$',fontsize=10)
-    
-
-    ax2.scatter(valores_x, valores_y, color = 'darkviolet')
-    plt.errorbar(valores_x[0],valores_y[0],yerr=regr_azul.intercept_stderr*q,xerr=None,fmt='darkviolet', ecolor='darkviolet')
-    plt.errorbar(valores_x[1],valores_y[1],yerr=regr_verde.intercept_stderr*q,xerr=None,fmt='darkviolet', ecolor='darkviolet')
-    plt.errorbar(valores_x[2],valores_y[2],yerr=regr_amarillo.intercept_stderr*q,xerr=None,fmt='darkviolet', ecolor='darkviolet')
-    plt.errorbar(valores_x[3],valores_y[3],yerr=regr_rojo.intercept_stderr*q,xerr=None,fmt='darkviolet', ecolor='darkviolet')
-    regr_planck = stats.linregress(valores_x,valores_y)
-    Y_planck = valores_x*regr_planck.slope + regr_planck.intercept
-    plt.plot(valores_x, Y_planck, '--',color='purple', label='Ajuste')
-    
-    #plt.errorbar(valores_x[0],valores_y[0],yerr=regr_azul.intercept_stderr,xerr=None,fmt='lime', ecolor='lime')
-    
-    
-    print(f"planck: {regr_planck.slope} +- {regr_planck.stderr}, {-regr_planck.intercept} +- {regr_planck.intercept_stderr}.")
-    plt.savefig("planck.png")
+    plt.savefig('calibracion_hidrogeno.png', dpi=300, bbox_inches='tight')
     plt.show()
+    
+    # Resultado: m = 0.35, b = -6.83
+    
+    func_conversion = lambda x: x*regression.slope + regression.intercept
+    # recibe pixeles y devuelve la frecuencia equivalente en nm
+    
+    
+    
+    
+    
+    
+    # 2. Hallar constante de Rydberg:
+    
+    xvalues_list = np.array([0,1/4 - 1/36,1/4 - 1/25,1/4 - 1/9])
+    lambdas_list = 1/(func_conversion(np.array([1190,1337,1845]))*1e-9) # y
+    lambdas_list = lambdas_list.tolist()
+    lambdas_list.insert(0,0)
 
+    
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.set_xlabel(r'$\frac{1}{2^2} - \frac{1}{n^2}$',fontsize=10)
+    ax.set_ylabel(r'$\frac{1}{\lambda}[m]$',fontsize=10)
+    ax.set_title(r'Constante de Rydberg',fontsize=10)
+
+    ax.scatter(xvalues_list, lambdas_list, c='k',s=10)
+    #plt.errorbar(i_rojo,v_rojo,yerr=error_y,xerr=error_x,fmt='r', ecolor='r')
+    regression = stats.linregress(xvalues_list,lambdas_list)
+    Y_regression = xvalues_list*regression.slope + regression.intercept
+    plt.plot(xvalues_list, Y_regression, '--',color='darkred', label=r'$m    = 10702743.36 m^-1$   ')
+    
+    print(f'm = {regression.slope}, b = {regression.intercept}')
+    
+     
+    plt.legend()
+    plt.savefig('rydberg.png', dpi=300, bbox_inches='tight')
+    plt.show()
+    
+   
+   
+    # Línea amarilla doble del mercurio
+    print(f'Las lineas dobles del mercurio son {func_conversion(382)}, {func_conversion(384)}')
     
 
 if __name__ == "__main__":
